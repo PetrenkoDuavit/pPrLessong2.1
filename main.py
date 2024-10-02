baf0ef5cbd756799ddfac294e0686305acc2e6d1
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-10-02 07:26:28 +0300

    homeworck2.2

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+
+#homeworck 2.1-------------------------
+
 number = int(input("Enter number:"))
 n1 = number // 1000
 n2 = number // 100 % 10
@@ -25,3 +28,12 @@ print(n2)
 print(n3)
 print(n4)
 
+#homeworck 2.2-------------------------
+
+number = int(input("Enter number:"))
+n1 = number // 10000
+n2 = number // 1000 %10
+n3 = number // 100 % 10
+n4 = number % 100 // 10
+n5 = number % 10
+print(n5*10000+n4*1000+n3*100+n2*10+n1)
